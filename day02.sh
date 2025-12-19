ce58647f87674cd9bac94c32937379b7af621a1a
ogmobot <ogmobot@github.com>
2025-12-19 22:47:08 +1100

    Day 1 (part 1) in Heol

---
+++ day02.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Heol can't always deal with long lists.
+# Rather than attempting to read a 1000-element-long list,
+# go through the input line-by-line.
+heol_in=/tmp/data.pipe_in
+rm -f $heol_in
+mkfifo $heol_in
+
+# If heol is upset that there's no available data,
+# this will ensure pipe stays open.
+#sleep 1 > $heol_in &
+
+# Count the number of #t (safe) the program outputs
+heol 2>&1 < $heol_in | grep -c "#t$" &
+heol_pid=$!
+# Load program file
+cat day02.heol > $heol_in
+# Send each line of file to program
+while read p; do
+    echo "(safe? '(" $p "))" > $heol_in
+done < input02.txt
+wait $heol_pid
