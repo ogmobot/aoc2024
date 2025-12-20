f6d1cad6bb40a0bbb01814b3a383b513bd1a4b1c
ogmobot <ogmobot@github.com>
2025-12-20 17:07:46 +1100

    Day 2 (WIP): definitely recursion issues

--- day02.sh
+++ day02.sh
@@ -19,8 +19,13 @@ done
 grep -L -o "[01]\s\.\s[01]" $out_dir/*
 # 45 44 42 41 39 36 33 29
 # 58 56 55 54 52 51 50 51
-# In both cases, it's the value with index 7...
+#  5  7 10 12 14 16 18 23
+# 53 54 57 60 61 62 63 67
+# In all cases, it's the value with index 7...
+# Recursive stack overflow issue?
+# (It only happens for whichever function is called second within safeish)
+ls $out_dir | wc -l
 cat $out_dir/* | grep -o "[01]\s\.\s[01]" | \
     awk -F " . " \
-        '{ p1 += $1; p2 += $2; echo $0 } \
-        END { printf("%d %d\n", p1, p2) }'
+        '{ p1 += $1; p2 += $2 } \
+        END { print p1; print p2 }'
